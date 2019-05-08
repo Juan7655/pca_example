59ebefddcd93a3d42585c131f3273d1509807cd4
Juan David <david.rodriguez@endava.com>
2019-05-08 14:53:10 -0500

    improved type-hints for function parameters

--- Utils.py
+++ Utils.py
@@ -1,4 +1,5 @@
 import types
+from typing import Callable
 
 import matplotlib.pyplot as plt
 from pandas import DataFrame
@@ -6,18 +7,19 @@ from pandas import DataFrame
 
 def plot_graph(fun: types.FunctionType):
     def plotter(*args, **kwargs):
-            set1, set2 = fun(*args, **kwargs)
-            plt.plot(set1[0], set1[1], zorder=2, c=kwargs['color'], linewidth=.5)
-            plt.scatter(set2[0], set2[1], zorder=3, c=kwargs['color'], s=10)
-            if kwargs['draw']:
-                plt.xlabel('x')
-                plt.ylabel('y')
-                plt.show()
+        set1, set2 = fun(*args, **kwargs)
+        plt.plot(set1[0], set1[1], zorder=2, c=kwargs['color'], linewidth=.5)
+        plt.scatter(set2[0], set2[1], zorder=3, c=kwargs['color'], s=10)
+        if kwargs['draw']:
+            plt.xlabel('x')
+            plt.ylabel('y')
+            plt.show()
+
     return plotter
 
 
 @plot_graph
-def plot_rotated(df: DataFrame, lin_reg: types.FunctionType, *args, **kwargs):
+def plot_rotated(df: DataFrame, lin_reg: Callable[[DataFrame], tuple], *args, **kwargs):
     if df.shape[1] > 1:
         y_values = df[df.columns[1]]
         m, b = lin_reg(df)
